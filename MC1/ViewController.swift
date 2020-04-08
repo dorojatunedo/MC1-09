38c53aa4447701919a7b094779a01cca31e7af13
Alvian <alvigoza@gmail.com>
2020-04-08 14:38:54 +0700

    Initial Commit

---
+++ MC1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC1
+//
+//  Created by Alvian Gozali on 08/04/20.
+//  Copyright © 2020 Alvian Gozali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
